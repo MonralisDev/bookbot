b41132ec1fb3585280c256dfa12ba30c2150f11a
M <M>
2025-03-04 14:53:51 +0200

    Додав підрахунок символів у stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,8 +7,12 @@ def get_book_text(filepath):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
+
     num_words = count_words(book_text)
     print(f"{num_words} words found in the document")
+
+    char_counts = count_characters(book_text)
+    print(char_counts)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    char_count = {}
+    text = text.lower()
+
+    for chair in text:
+        if chair in char_count:
+            char_count[chair] += 1
+        else:
+            char_count[chair] = 1
+    return char_count
