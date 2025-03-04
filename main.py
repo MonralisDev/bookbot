85fa05fef12d7d481901ec14279dadb84271eb92
M <M>
2025-03-04 15:03:26 +0200

    Додав сортування символів і красивий вивід

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
 
     num_words = count_words(book_text)
-    print(f"{num_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
 
     char_counts = count_characters(book_text)
-    print(char_counts)
+    sorted_chars = sort_characters(char_counts)
+
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['count']}")
+
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -6,9 +6,15 @@ def count_characters(text):
     char_count = {}
     text = text.lower()
 
-    for chair in text:
-        if chair in char_count:
-            char_count[chair] += 1
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
         else:
-            char_count[chair] = 1
+            char_count[char] = 1
+
     return char_count
+
+def sort_characters(char_dict):
+    sorted_list = [{"char": char, "count": count} for char, count in char_dict.items() if char.isalpha()]
+    sorted_list.sort(key=lambda d: d["count"], reverse=True)  # Сортуємо за кількістю входжень
+    return sorted_list
