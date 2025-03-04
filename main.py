973bdee3fb1ec2b67ea709b2e6f751e846d7f1af
M <M>
2025-03-04 22:55:09 +0200

    a description of your changes here

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import sys
 from stats import count_words, count_characters, sort_characters
 
 def get_book_text(filepath):
-    with open(filepath) as f:
+    with open(filepath, encoding="utf-8") as f:
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
+    # Перевіряємо, чи переданий аргумент (шлях до книги)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]  # Отримуємо шлях до файлу з аргументу
+
+    try:
+        book_text = get_book_text(book_path)
+    except FileNotFoundError:
+        print(f"Error: File '{book_path}' not found.")
+        sys.exit(1)
 
     num_words = count_words(book_text)
     print("============ BOOKBOT ============")
